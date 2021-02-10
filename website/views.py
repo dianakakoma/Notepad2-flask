695f550dc2d5cbb31cbc32c97793e0f784acf438
dianakakoma <dikakoma@gmail.com>
2021-02-09 19:19:55 -0600

    created the html page templates.

--- website/views.py
+++ website/views.py
@@ -1,8 +1,12 @@
 #routes except for authentication
-from flask import Blueprint
+from flask import Blueprint, render_template
 
 views = Blueprint('views', __name__)
 
 @views.route('/')
 def home():
-    return "<h1>Test</h1>"
+    return render_template("index.html")
+
+@views.route('/sign-up')
+def sign_up():
+    return render_template("sign_up.html")
