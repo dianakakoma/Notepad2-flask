f22b296659c4ae51476b32f5b7f4b0c87342472a
dianakakoma <dikakoma@gmail.com>
2021-02-09 21:01:19 -0600

    set SQLAlchemy database

--- website/auth.py
+++ website/auth.py
@@ -1,16 +1,26 @@
-from flask import Blueprint
+from flask import Blueprint, request
 
 auth = Blueprint('auth', __name__)
 
-@auth.route("/login")
+@auth.route("/login", methods=["GET","POST"])
 def login():
+    data = request.form
+    print(data)
     return "<p>Login</p>"
 
 @auth.route("/logout")
 def logout():
+
     return "<p>Logout</p>"
 
-@auth.route("/sign-up")
+@auth.route("/sign-up", methods=["GET","POST"])
 def sign_up():
+    if request.method == "POST":
+        email = request.form.get("email")
+        name = request.form.get("name")
+        password1 = request.form.get("password1")
+        password2 = request.form.get("password2")
+
+
     return "<p>Sign Up</p>"
 
--- website/views.py
+++ website/views.py
@@ -6,7 +6,12 @@ views = Blueprint('views', __name__)
 @views.route('/')
 def home():
     return render_template("index.html")
-
+#post request
 @views.route('/sign-up')
 def sign_up():
     return render_template("sign_up.html")
+
+#post request
+@views.route('/login')
+def login():
+    return render_template("login.html")
