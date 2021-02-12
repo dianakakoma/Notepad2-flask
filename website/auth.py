e787c0a582edad0b062bb992e5a613fbdc810e5e
dianakakoma <dikakoma@gmail.com>
2021-02-11 20:27:14 -0600

    completed login, and out functionality

--- website/auth.py
+++ website/auth.py
@@ -1,20 +1,35 @@
-from flask import Blueprint, render_template, request, redirect, url_for
+from flask import Blueprint, render_template, flash, request, redirect, url_for
 from .models import User
 from werkzeug.security import generate_password_hash, check_password_hash,check_password_hash
 from . import db
+from flask_login import login_user, login_required, logout_user, current_user
 
 auth = Blueprint('auth', __name__)
 
 @auth.route("/login", methods=["GET","POST"])
 def login():
-    data = request.form
-    print(data)
-    return "<p>Login</p>"
+    if request.method == "POST":
+        email = request.form.get("email")
+        password = request.form.get("password")
+
+        #query the database
+        user = User.query.filter_by(email=email).first()
+        if user:
+            if check_password_hash(user.password, password):
+                flash('Logged in successfully!', category = 'success')
+                login_user(user, remember=True)
+                return redirect('views.home')
+            else:
+                flash('Incorrect password, try again',category='error')
+        else:
+            flash('Email does not exist.', category = 'error')
+    return render_template("login.html", user=current_user)
 
 @auth.route("/logout")
+@login_required
 def logout():
-
-    return "<p>Logout</p>"
+    logout_user()
+    return redirect(url_for('auth.login'))
 
 @auth.route("/sign-up", methods=["GET","POST"])
 def sign_up():
@@ -27,6 +42,7 @@ def sign_up():
         new_user = User(email=email, name=name, password= generate_password_hash(password1, method='sha256'))
         db.session.add(new_user)
         db.session.commit()
-        return redirect(url_for('views.home'))
-    return render_template("sign_up.html")
+        login_user(user, remember=True)
+        return redirect('views.home')
+    return render_template("sign_up.html", user=current_user)
 
--- website/views.py
+++ website/views.py
@@ -1,12 +1,15 @@
 #routes except for authentication
 from flask import Blueprint, render_template
+from flask_login import login_required,  current_user
 
 views = Blueprint('views', __name__)
 
 @views.route('/')
+@login_required
 def home():
     return render_template("index.html")
 #post request
+
 @views.route('/sign-up')
 def sign_up():
     return render_template("sign_up.html")
